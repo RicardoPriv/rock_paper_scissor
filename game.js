88f02fdfddaf6be2847f6149e5a7f946427ec084
Ricardo7720 <a103996@uminho.pt>
2025-02-24 20:56:10 +0000

    Added: - Added a design for the game - Included borders - Borders and results appear after user makes choice

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ const choices = {
     2: "Scissors"
 };
 
+const results = {
+    0: "Draw",
+    1: "Lose",
+    2: "Win"
+};
+
 function get_ai_choice() {
     let choice = (Math.trunc(Math.random() * 10) % 3);
     return choice    
@@ -14,19 +20,27 @@ function who_wins(user_choice, ai_choice) {
         return 0;
     }
     else if ((ai_choice - user_choice == 1) || (user_choice - ai_choice == 2)) {
-        return -1;
+        return 1;
     }
     else {
-        return 1;
+        return 2;
     }
 }
 
 function show_results(user_choice) {
     let ai_choice = get_ai_choice();
-    let results = who_wins(user_choice, ai_choice);
+    let result = who_wins(user_choice, ai_choice);
     console.log(choices[user_choice]);
     console.log(choices[ai_choice]);
-    console.log(results);
+    console.log(results[result]);
+
+    document.getElementById("user-choice").innerHTML = "You chose " + choices[user_choice]
+    document.getElementById("ai-choice").innerHTML = "The ai chose " + choices[ai_choice];
+    document.getElementById("results").innerHTML = "You " + results[result];
+
+    document.getElementById("user-choice").style.border = "2px solid black";
+    document.getElementById("ai-choice").style.border = "2px solid black";
+    document.getElementById("results").style.border = "2px solid black";
 }
 
 document.getElementById("rock-button").addEventListener("click", function() {
